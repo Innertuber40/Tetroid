d67d33cd5e45544ecba59224e89fb141368c63c0
Brandon U <bchen00@stuy.edu>
2019-01-16 21:29:07 -0800

    merge changes

--- Grapple.java
+++ Grapple.java
@@ -1,12 +1,10 @@
-public class Grapple{
-  private int x;
-  private int y;
-  private boolean exists;
-
-  public Grapple(int startX, int startY, Terminal terminal){
+public class Grapple extend Bullet{
+/**
+  public Grapple(int startX, int startY, Player owner, Terminal terminal, int direction){
     x = startX;
     y = startY;
     terminal.moveCursor(x,y++);
-    terminal.putCharacter(' '); // replace space with ^
+    terminal.putCharacter('U+00A5'); // replace space with ^
   }
+  **
 }
