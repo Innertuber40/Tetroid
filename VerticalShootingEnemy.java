3242a7daf8fe5659ba410187f87e4ed6f851fb23
Brandon U <bchen00@stuy.edu>
2019-01-17 21:42:54 -0800

    Finished VerticalShootingEnemy and tested in tetroid.java

---
+++ VerticalShootingEnemy.java
@@ -0,0 +1,28 @@
+import com.googlecode.lanterna.terminal.Terminal;
+public class VerticalShootingEnemy extends Enemy{
+
+  public VerticalShootingEnemy(int startX, int startY, int h, Terminal t){
+    //health = h;
+    //x = startX;
+    //y = startY;
+    //exists = true;
+    //terminal = t;
+    super(startX,startY,h,t);
+    super.getTerminal().moveCursor(super.getX(),super.getY());
+    super.getTerminal().putCharacter('\u0029'); //back of the head
+    super.getTerminal().moveCursor(super.getX()-1,super.getY());
+    super.getTerminal().putCharacter('\u002A'); //eye
+    super.getTerminal().moveCursor(super.getX()-2,super.getY());
+    super.getTerminal().putCharacter('\u003C'); //beak
+    super.getTerminal().moveCursor(super.getX(),super.getY()+1);
+    super.getTerminal().putCharacter('\u004C'); //wing
+    super.getTerminal().moveCursor(super.getX()-1,super.getY()+1);
+    super.getTerminal().putCharacter('\u005F'); //stomach
+    super.getTerminal().moveCursor(super.getX()-2,super.getY()+1);
+    super.getTerminal().putCharacter('\u0028'); //chest
+    super.getTerminal().moveCursor(super.getX()+1,super.getY()+1);
+    super.getTerminal().putCharacter('\u0029'); //butt
+    super.getTerminal().moveCursor(super.getX()+2,super.getY()+1);
+    super.getTerminal().putCharacter('\u2013'); //tail
+  }
+}
