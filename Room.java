8f8b883e426744c15f241e5156136c97f811e46b
bstringham <bstringham@stuy.edu>
2019-01-22 17:55:46 -0500

    Added comments

--- Room.java
+++ Room.java
@@ -14,9 +14,9 @@ import com.googlecode.lanterna.input.KeyMappingProfile;
 import java.util.ArrayList;
 
 public class Room {
-  private int roomNumber;
-  private Pixel[][] design;
-  private ArrayList<Integer> entrances;
+  private int roomNumber; // useless, just helps keep track when initializing
+  private Pixel[][] design; // how the room looks
+  private ArrayList<Integer> entrances; // the places that lead to other rooms
 
   public Room(int num, Pixel[][] des, ArrayList<Integer> entrs) {
     roomNumber = num;
@@ -24,14 +24,14 @@ public class Room {
     entrances = entrs;
   }
 
-  public boolean isAPixel(int x, int y) {
+  public boolean isAPixel(int x, int y) { // tells whatever is asking whether a given part of the room can be interacted with
     if (design[x][y] != null) {
       return true;
     } else {
       return false;
     }
   }
-  public ArrayList<Integer> entrances() {
+  public ArrayList<Integer> entrances() { // enables the entrances list to be accessed
 	  return entrances;
   }
 }
